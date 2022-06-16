a2a5b7bb5ada9e92d21b3ab2abe1aa64da0503f7
James Yang <jproyang10@gmail.com>
2022-06-16 14:11:41 -0500

    Added changes

--- src/main/java/helloWorld.java
+++ src/main/java/helloWorld.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class helloWorld {
 
     public static void main(String[] args) {
-
         System.out.println("Hello World");
+
+        Scanner sc = new Scanner(System.in); //Creates a scanner obejct
+        System.out.println("Enter your name");
+        String user = sc.nextLine(); //Reads users input
+        System.out.println("Nice to meet you " + user);
 
     }
 }
